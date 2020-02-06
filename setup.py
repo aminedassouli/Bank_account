2d01ed92f552998a3d396bb4e79e7290c0c04520
aminedassouli <aminedassouli@gmail.com>
2020-02-06 01:08:59 +0100

    Starting the project

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Importations
+# ------------
+from setuptools import find_packages, setup
+
+
+# Package meta-data
+# -----------------
+NAME = 'Bank Account or Service complaints'
+DESCRIPTION = ''
+EMAIL = 'aminedassouli@gmail.com'
+AUTHOR = 'Mohamed Amine Dassouli'
+REQUIRES_PYTHON = '>=3.6.0'
+
+
+# Required packages
+# -----------------
+REQUIRED = [
+    "pandas",
+    "notebook"
+]
+
+
+# Where the magic happens
+# -----------------------
+setup(
+    name=NAME,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(),
+    install_requires=REQUIRED,
+    include_package_data=True
+)
